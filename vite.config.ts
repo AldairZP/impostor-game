cd6808e4017f87eb40306dfb99cb197cf136ce2c
AldairZP <jonathan.aldair.zp@gmail.com>
2025-12-10 21:53:49 -0600

    feat: add PWA support with vite-plugin-pwa and update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Mi App React",
+        short_name: "App",
+        start_url: "/",
+        display: "standalone",
+        icons: [],
+      },
+    }),
+  ],
 });
